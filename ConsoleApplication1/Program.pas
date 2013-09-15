5cc894be0ce7d1c12f8939f0876ff216d7d639d0
John Moshakis <john@moshakis.com>
2013-09-14 20:32:55 -0400

    1st push

---
+++ ConsoleApplication1/Program.pas
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApplication1;
+
+interface
+
+uses
+  ServiceBus.Messages,
+  System.Linq, 
+  NServiceBus;
+
+type
+  ConsoleApp = class
+  public
+    class method Main(args: array of String);
+  end;
+
+implementation
+
+class method ConsoleApp.Main(args: array of String);
+begin
+  var bus := Configure
+  .With
+  .DefaultBuilder
+  .UseTransport<RabbitMQ>()
+  .UnicastBus()
+  .SendOnly
+  ;
+
+  bus.Send(new DomainMessage);
+end;
+
+end.
